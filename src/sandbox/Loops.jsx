8848eda4e78684093b3f750ec96396763c815c77
Victor Yampolsky <Victor.Yampolsky@cust2mate.com>
2023-11-27 21:08:59 +0200

    Lesson 4: if

--- src/sandbox/Loops.jsx
+++ src/sandbox/Loops.jsx
@@ -1,11 +1,15 @@
 import Box from '@mui/material/Box';
 
 const Loops = () => {
-    const arrOfStrings = ['one', 'two', 'three', 'four', 'five'];
+    // const arrOfStrings = ['one', 'two', 'three', 'four', 'five'];
+    const arrOfStrings = [];
+    if(!arrOfStrings.length) {
+        return <div>There are no strings</div>
+    }
     return (
-        <Box m={2}>
+        <Box m={2} >
             {arrOfStrings.map((str, index, arr) => {
-                return <div key={index} m={2}>{str}</div>
+                return <div  key={index} m={2}>{str}</div>
             })}
         </Box>
     )
