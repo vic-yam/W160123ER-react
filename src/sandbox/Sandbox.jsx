ae944080c3342fdcca22cbf24b0584d1ba893b99
Victor Yampolsky <Victor.Yampolsky@cust2mate.com>
2023-11-27 18:54:30 +0200

    Lesson 4: two keys props

--- src/sandbox/Sandbox.jsx
+++ src/sandbox/Sandbox.jsx
@@ -1,12 +1,14 @@
 import MuiSandbox from "./mui-sandbox/MuiSandbox";
 import FatherPropsObject from "./props/props-object/FatherPropsObject";
 import FatherPropsString from "./props/props-string/FatherPropsString";
+import FatherPropsTwoKeys from "./props/props-two-keys/FatherPropsTwoKeys";
 
 const Sandbox = () => (
     <>
         {/* <MuiSandbox /> */}
         {/* <FatherPropsString></FatherPropsString> */}
-        <FatherPropsObject></FatherPropsObject>
+        {/* <FatherPropsObject></FatherPropsObject> */}
+        <FatherPropsTwoKeys></FatherPropsTwoKeys>
     </>
 )
 
